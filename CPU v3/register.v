6c412c12bbde7696b08e73a13a0d95d70bc07f33
Varun Jain <vjrainuuu@gmail.com>
2025-10-09 19:59:50 +0530

    Mainreg Verification complete

--- CPU v3/register.v
+++ CPU v3/register.v
@@ -2,5 +2,5 @@ module register(IN, LOAD, CLK, OUT, PRESET_N, CLEAR_N);
 	input IN, LOAD, CLK, PRESET_N, CLEAR_N;
 	output OUT;
 	assign D = LOAD ? IN : OUT;
-	DFF my_dff(.D(D), .CLK(CLK), .PRN(PRESET_N), .CLRN(CLEAR_N), .Q(OUT));
+	dff my_dff(.d(D), .clk(CLK), .prn(PRESET_N), .clrn(CLEAR_N), .q(OUT));
 endmodule
