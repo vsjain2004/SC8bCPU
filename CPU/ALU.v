9ec5d72af2aced1c07de9aaf3f664bdd955818e8
Varun Jain <vjrainuuu@gmail.com>
2026-02-12 11:39:57 +0530

    Convert ALU to Verilog

--- CPU/ALU.v
+++ CPU/ALU.v
@@ -1,132 +1,24 @@
-// Copyright (C) 2025  Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions 
-// and other software and tools, and any partner logic 
-// functions, and any output files from any of the foregoing 
-// (including device programming or simulation files), and any 
-// associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License 
-// Subscription Agreement, the Altera Quartus Prime License Agreement,
-// the Altera IP License Agreement, or other applicable license
-// agreement, including, without limitation, that your use is for
-// the sole purpose of programming logic devices manufactured by
-// Altera and sold by Altera or its authorized distributors.  Please
-// refer to the Altera Software License Subscription Agreements 
-// on the Quartus Prime software download page.
+module ALU(ADD_SUB, Y, X, AS2, AS1, AS0, ZF, CF, OF, NF, Result);
 
-// PROGRAM		"Quartus Prime"
-// VERSION		"Version 24.1std.0 Build 1077 03/04/2025 SC Lite Edition"
-// CREATED		"Mon Oct  6 11:12:49 2025"
-
-module ALU(
-	AS1,
-	AS0,
-	AS2,
-	ADD_SUB,
-	X,
-	Y,
-	CF,
-	OF,
-	NF,
-	ZF,
-	Result
-);
-
-
-input wire	AS1;
-input wire	AS0;
-input wire	AS2;
-input wire	ADD_SUB;
-input wire	[7:0] X;
-input wire	[7:0] Y;
-output wire	CF;
-output wire	OF;
-output wire	NF;
-output wire	ZF;
-output wire	[7:0] Result;
-
-wire	[7:0] N;
-wire	[7:0] O;
-wire	[7:0] SYNTHESIZED_WIRE_0;
-wire	[7:0] SYNTHESIZED_WIRE_1;
-wire	[7:0] SYNTHESIZED_WIRE_2;
-wire	[7:0] SYNTHESIZED_WIRE_3;
-wire	[7:0] SYNTHESIZED_WIRE_4;
-wire	[7:0] SYNTHESIZED_WIRE_5;
-wire	[7:0] SYNTHESIZED_WIRE_6;
-
-wire	[2:0] GDFX_TEMP_SIGNAL_0;
-
-
-assign	GDFX_TEMP_SIGNAL_0 = {AS2,AS1,AS0};
-
-
-NOT_8_bit	b2v_inst(
-	.In(X),
-	.Out(SYNTHESIZED_WIRE_3));
-
-
-CLA_8_bit	b2v_inst1(
-	.Cin(ADD_SUB),
-	.X(X),
-	.Y(Y),
-	.Cout(CF),
-	.Ov(OF),
-	.S(N));
-
-
-AND_8_bit	b2v_inst2(
-	.Control(X),
-	.In(Y),
-	.Out(SYNTHESIZED_WIRE_0));
-
-
-OR_8_bit	b2v_inst3(
-	.Control(X),
-	.In(Y),
-	.Out(SYNTHESIZED_WIRE_1));
-
-
-XOR_8_bit	b2v_inst4(
-	.Control(X),
-	.In(Y),
-	.Out(SYNTHESIZED_WIRE_2));
-
-assign	ZF = ~(N[0] | N[2] | N[1] | N[3] | N[5] | N[4] | N[6] | N[7]);
-
-
-Logic_Shifter	b2v_inst6(
-	.ShiftDir(ADD_SUB),
-	.IN(X),
-	.ShiftAmt(Y[2:0]),
-	.Out(SYNTHESIZED_WIRE_4));
-
-
-Arithmetic_Shifter	b2v_inst7(
-	.IN(X),
-	.ShiftAmt(Y[2:0]),
-	.Out(SYNTHESIZED_WIRE_5));
-
-
-Circular_Shifter	b2v_inst8(
-	.ShiftDir(ADD_SUB),
-	.IN(X),
-	.ShiftAmt(Y[2:0]),
-	.Out(SYNTHESIZED_WIRE_6));
-
-
-busmux_8_to_1	b2v_inst9(
-	.S(GDFX_TEMP_SIGNAL_0),
-	.W0(N),
-	.W1(SYNTHESIZED_WIRE_0),
-	.W2(SYNTHESIZED_WIRE_1),
-	.W3(SYNTHESIZED_WIRE_2),
-	.W4(SYNTHESIZED_WIRE_3),
-	.W5(SYNTHESIZED_WIRE_4),
-	.W6(SYNTHESIZED_WIRE_5),
-	.W7(SYNTHESIZED_WIRE_6),
-	.F(O));
-
-assign	NF = N[7];
-assign	Result = O;
-
+	input ADD_SUB, AS2, AS1, AS0;
+	input [7:0] Y, X;
+	output ZF, CF, OF, NF;
+	output [7:0] Result;
+	
+	wire [7:0] oAdd, oAnd, oOr, oXor, oNot, oLS, oAS, oCS;
+	
+	AddSub8b adder(.Cin(ADD_SUB), .X(X), .Y(Y), .S(oAdd), .Cout(CF), .Ov(OF));
+	AND_8_bit ander(.In(Y), .Control(X), .Out(oAnd));
+	OR_8_bit orer(.In(Y), .Control(X), .Out(oOr));
+	XOR_8_bit xorer(.In(Y), .Control(X), .Out(oXor));
+	NOT_8_bit noter(.In(X), .Out(oNot));
+	Logic_Shifter ls(.IN(X), .ShiftAmt(Y[2:0]), .ShiftDir(ADD_SUB), .Out(oLS));
+	Arithmetic_Shifter as(.IN(X), .ShiftAmt(Y[2:0]), .Out(oAS));
+	Circular_Shifter cs(.IN(X), .ShiftAmt(Y[2:0]), .ShiftDir(ADD_SUB), .Out(oCS));
+	
+	busmux_8_to_1 mux1(.S({AS2, AS1, AS0}), .W0(oAdd), .W1(oAnd), .W2(oOr), .W3(oXor), .W4(oNot), .W5(oLS), .W6(oAS), .W7(oCS), .F(Result));
+	
+	assign NF = oAdd[7];
+	assign ZF = ~(oAdd[0] | oAdd[1] | oAdd[2] | oAdd[3] | oAdd[4] | oAdd[5] | oAdd[6] | oAdd[7]);
+	
 endmodule
