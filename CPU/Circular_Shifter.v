8316aa6bd58a70abba4c25a9c3af80eb19bbf0b0
Varun Jain <vjrainuuu@gmail.com>
2026-02-21 19:56:49 +0530

    Complete Green Card and fix Circular Shifter

--- CPU/Circular_Shifter.v
+++ CPU/Circular_Shifter.v
@@ -6,5 +6,7 @@ module Circular_Shifter #(parameter SHIFT_BITS = 5) (IN, ShiftAmt, ShiftDir, Out
 	input ShiftDir;
 	output [N-1:0] Out;
 
-	assign Out = {IN, IN} >> (N - ShiftAmt);
+	assign Out = ShiftDir ? {IN, IN} >> ShiftAmt : {IN, IN} >> (N - ShiftAmt);
+
+	// 0101 1011 -> 1010 1101 = CSR by 1 ==> 01011011{01011011} Shift Right by 1 ==> 0101101{10101101}1
 endmodule
